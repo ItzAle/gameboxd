077a25ba30bfe6a6ce693e7f57bd365cb593803c
itzAle <alexcs9230@gmail.com>
2024-08-22 01:19:34 +0200

    change profile, add new features (no one is working)

--- src/Components/UserProfile/UserProfile.jsx
+++ src/Components/UserProfile/UserProfile.jsx
@@ -12,6 +12,7 @@ import {
   where,
   getDocs,
   writeBatch,
+  serverTimestamp,
 } from "firebase/firestore";
 import { db } from "../../../lib/firebase";
 import jsonp from "jsonp";
@@ -77,6 +78,99 @@ const Modal = ({
   );
 };
 
+// Modal para añadir juegos favoritos
+const AddFavoriteGamesModal = ({
+  isOpen,
+  onClose,
+  onAdd,
+  favoriteGames,
+  setFavoriteGames,
+}) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = async () => {
+    // Lógica para buscar juegos en la API
+    const params = {
+      api_key: "54a0e172e4af5165c21d0517ca55f7c8f3d34aab",
+      format: "jsonp",
+      json_callback: "jsonpCallback",
+      filter: `name:${searchTerm}`,
+    };
+
+    const urlParams = new URLSearchParams(params).toString();
+    const apiUrlWithParams = `https://www.giantbomb.com/api/games/?${urlParams}`;
+
+    jsonp(apiUrlWithParams, { param: "json_callback" }, (err, data) => {
+      if (err) {
+        console.error("Error fetching game data:", err);
+      } else {
+        setSearchResults(data.results);
+      }
+    });
+  };
+
+  const handleAddGame = (game) => {
+    if (favoriteGames.length < 4) {
+      setFavoriteGames([...favoriteGames, game]);
+    }
+  };
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex items-center justify-center">
+      <div className="bg-white p-6 rounded shadow-lg max-w-md w-full">
+        <h2 className="text-xl font-semibold mb-4">Add Favorite Games</h2>
+        <div className="mb-4">
+          <input
+            type="text"
+            placeholder="Search for games"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="p-2 border rounded w-full"
+          />
+          <button
+            onClick={handleSearch}
+            className="bg-blue-500 text-white px-4 py-2 rounded-md mt-2"
+          >
+            Search
+          </button>
+        </div>
+        <div className="mb-4">
+          <ul className="space-y-2">
+            {searchResults.map((game) => (
+              <li key={game.id} className="flex justify-between items-center">
+                <span>{game.name}</span>
+                <button
+                  onClick={() => handleAddGame(game)}
+                  className="bg-green-500 text-white px-2 py-1 rounded-md"
+                >
+                  Add
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="flex justify-end space-x-4">
+          <button
+            onClick={onClose}
+            className="bg-gray-300 text-gray-800 px-4 py-2 rounded-md"
+          >
+            Cancel
+          </button>
+          <button
+            onClick={onAdd}
+            className="bg-blue-500 text-white px-4 py-2 rounded-md"
+          >
+            Save
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default function UserProfile() {
   const { data: session } = useSession();
   const [userProfile, setUserProfile] = useState(null);
@@ -84,6 +178,8 @@ export default function UserProfile() {
   const [editing, setEditing] = useState(false);
   const [bio, setBio] = useState("");
   const [profilePicture, setProfilePicture] = useState("");
+  const [favoriteGames, setFavoriteGames] = useState([]);
+  const [addingFavoriteGames, setAddingFavoriteGames] = useState(false);
   const user = session?.user;
   const apiUrl = "https://www.giantbomb.com/api/games/";
 
@@ -129,10 +225,11 @@ export default function UserProfile() {
           setUserProfile(userData);
           setBio(userData.bio || "");
           setProfilePicture(userData.profilePicture || "");
+          setFavoriteGames(userData.favoriteGames || []);
 
           // Obtener las carátulas de los juegos
           const newCovers = {};
-          for (const game of userData.likedGames) {
+          for (const game of userData.favoriteGames) {
             if (game.gameId) {
               try {
                 const gameData = await fetchGameById(game.gameId);
@@ -147,7 +244,7 @@ export default function UserProfile() {
           }
           setCovers(newCovers);
         } else {
-          setUserProfile({ likedGames: [], reviews: [] });
+          setUserProfile({ reviews: [], favoriteGames: [] });
         }
       } catch (error) {
         console.error("Error fetching user profile:", error);
@@ -168,6 +265,8 @@ export default function UserProfile() {
       batch.update(userRef, {
         bio,
         profilePicture,
+        favoriteGames,
+        updatedAt: serverTimestamp(),
       });
 
       await batch.commit();
@@ -177,6 +276,8 @@ export default function UserProfile() {
         ...prevProfile,
         bio,
         profilePicture,
+        favoriteGames,
+        updatedAt: new Date(),
       }));
 
       setEditing(false);
@@ -192,6 +293,15 @@ export default function UserProfile() {
       setProfilePicture(userProfile.profilePicture || "");
     }
     setEditing(true);
+  };
+
+  const handleAddFavoriteGames = () => {
+    setAddingFavoriteGames(true);
+  };
+
+  const handleSaveFavoriteGames = () => {
+    setAddingFavoriteGames(false);
+    handleSaveProfile();
   };
 
   if (!user) {
@@ -241,7 +351,7 @@ export default function UserProfile() {
             <h2 className="text-2xl font-semibold mb-2">Liked Games</h2>
             {userProfile.likedGames && userProfile.likedGames.length > 0 ? (
               <ul className="grid grid-cols-2 sm:grid-cols-3 gap-4 overflow-y-auto max-h-64">
-                {userProfile.likedGames.map((game) => (
+                {userProfile.likedGames.slice(0, 4).map((game) => (
                   <li key={game.gameId} className="mb-2">
                     <Link href={`/games/${game.gameId}`} className="group">
                       <div className="relative overflow-hidden rounded-lg shadow-lg transition-all duration-300 transform hover:scale-105 h-full">
@@ -269,37 +379,146 @@ export default function UserProfile() {
             ) : (
               <p className="text-lg">You have not liked any games yet.</p>
             )}
+            {userProfile.likedGames.length > 4 && (
+              <Link href={`/profile/${user.name}/liked-games`} className="text-blue-500 hover:underline">
+                View all liked games
+              </Link>
+            )}
+          </div>
+
+          {/* Favorite Games Section */}
+          <div className="mb-8 p-4 border border-gray-300 rounded bg-white shadow-md">
+            <h2 className="text-2xl font-semibold mb-2">Favorite Games</h2>
+            {favoriteGames.length > 0 ? (
+              <ul className="grid grid-cols-2 sm:grid-cols-4 gap-4">
+                {favoriteGames.map((game) => (
+                  <li key={game.id} className="mb-2">
+                    <Link href={`/games/${game.id}`} className="group">
+                      <div className="relative overflow-hidden rounded-lg shadow-lg transition-all duration-300 transform hover:scale-105 h-full">
+                        <div className="aspect-w-3 aspect-h-4 bg-gray-200">
+                          <img
+                            src={
+                              covers[game.id] ||
+                              "/path/to/placeholder-image.png"
+                            }
+                            alt={game.name}
+                            className="object-cover w-full h-full"
+                          />
+                        </div>
+                        <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
+                        <div className="absolute bottom-0 left-0 right-0 p-4 text-white transform translate-y-full group-hover:translate-y-0 transition-transform duration-300">
+                          <p className="text-center font-medium">{game.name}</p>
+                        </div>
+                      </div>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <div className="flex justify-center items-center h-32">
+                <button
+                  onClick={handleAddFavoriteGames}
+                  className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 transition duration-300"
+                >
+                  + Add Favorite Games
+                </button>
+              </div>
+            )}
           </div>
         </div>
 
-        {/* Reviews Section */}
+        {/* Recent Activity Section */}
         <div className="md:col-span-2">
           <div className="mb-8 p-4 border border-gray-300 rounded bg-white shadow-md">
-            <h2 className="text-2xl font-semibold mb-2">Your Reviews</h2>
+            <div className="flex justify-between items-center mb-2">
+              <h2 className="text-2xl font-semibold">Recent Activity</h2>
+              <Link
+                href={`/profile/${user.name}/recent-activity`}
+                className="text-blue-500 hover:underline"
+              >
+                All
+              </Link>
+            </div>
+            {userProfile.recentActivity &&
+            userProfile.recentActivity.length > 0 ? (
+              <ul className="grid grid-cols-2 sm:grid-cols-4 gap-4">
+                {userProfile.recentActivity.slice(0, 4).map((activity) => (
+                  <li key={activity.gameId} className="mb-2">
+                    <Link href={`/games/${activity.gameId}`} className="group">
+                      <div className="relative overflow-hidden rounded-lg shadow-lg transition-all duration-300 transform hover:scale-105 h-full">
+                        <div className="aspect-w-3 aspect-h-4 bg-gray-200">
+                          <img
+                            src={
+                              covers[activity.gameId] ||
+                              "/path/to/placeholder-image.png"
+                            }
+                            alt={activity.gameName}
+                            className="object-cover w-full h-full"
+                          />
+                        </div>
+                        <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
+                        <div className="absolute bottom-0 left-0 right-0 p-4 text-white transform translate-y-full group-hover:translate-y-0 transition-transform duration-300">
+                          <p className="text-center font-medium">
+                            {activity.gameName}
+                          </p>
+                          <div className="flex justify-center space-x-2 mt-2">
+                            {activity.rating && (
+                              <span className="text-yellow-500">
+                                {"★".repeat(activity.rating)}
+                                {"☆".repeat(5 - activity.rating)}
+                              </span>
+                            )}
+                            {activity.review && (
+                              <span className="text-blue-500">Review</span>
+                            )}
+                            {activity.like && (
+                              <span className="text-red-500">♥</span>
+                            )}
+                          </div>
+                        </div>
+                      </div>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p className="text-lg">No recent activity.</p>
+            )}
+          </div>
+
+          {/* Reviews Section */}
+          <div className="mb-8 p-4 border border-gray-300 rounded bg-white shadow-md">
+            <div className="flex justify-between items-center mb-2">
+              <h2 className="text-2xl font-semibold">Recent Reviews</h2>
+              <Link
+                href={`/profile/${user.name}/reviews`}
+                className="text-blue-500 hover:underline"
+              >
+                More
+              </Link>
+            </div>
             {userProfile.reviews && userProfile.reviews.length > 0 ? (
               <ul className="space-y-4">
-                {userProfile.reviews
-                  .filter((review) => review.rating > 0)
-                  .map((review) => (
-                    <li
-                      key={review.gameId}
-                      className="p-4 border border-gray-300 rounded bg-white shadow-md hover:shadow-lg transition-shadow duration-300"
-                    >
-                      <Link href={`/games/${review.gameId}`}>
-                        <p className="text-xl font-bold hover:underline">
-                          {review.gameName}
-                        </p>
-                      </Link>
-                      <p className="text-yellow-500">
-                        {"★".repeat(review.rating)}
-                        {"☆".repeat(5 - review.rating)}
+                {userProfile.reviews.slice(0, 2).map((review) => (
+                  <li
+                    key={review.gameId}
+                    className="p-4 border border-gray-300 rounded bg-white shadow-md hover:shadow-lg transition-shadow duration-300"
+                  >
+                    <Link href={`/games/${review.gameId}`}>
+                      <p className="text-xl font-bold hover:underline">
+                        {review.gameName}
                       </p>
-                      <p className="mt-2">{review.comment}</p>
-                      {review.containsSpoilers && (
-                        <p className="text-red-500 mt-1">Contains Spoilers</p>
-                      )}
-                    </li>
-                  ))}
+                    </Link>
+                    <p className="text-yellow-500">
+                      {"★".repeat(review.rating)}
+                      {"☆".repeat(5 - review.rating)}
+                    </p>
+                    <p className="mt-2">{review.comment}</p>
+                    {review.containsSpoilers && (
+                      <p className="text-red-500 mt-1">Contains Spoilers</p>
+                    )}
+                  </li>
+                ))}
               </ul>
             ) : (
               <p className="text-lg">You have not written any reviews yet.</p>
@@ -318,6 +537,17 @@ export default function UserProfile() {
           setProfilePicture={setProfilePicture}
           bio={bio}
           profilePicture={profilePicture}
+        />
+      )}
+
+      {/* Modal para añadir juegos favoritos */}
+      {addingFavoriteGames && (
+        <AddFavoriteGamesModal
+          isOpen={addingFavoriteGames}
+          onClose={() => setAddingFavoriteGames(false)}
+          onAdd={handleSaveFavoriteGames}
+          favoriteGames={favoriteGames}
+          setFavoriteGames={setFavoriteGames}
         />
       )}
     </div>
---
+++ src/app/profile/[username]/recent-activity/page.js
@@ -0,0 +1,65 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from "../../../../../lib/firebase";
+import Link from "next/link";
+
+const RecentReviews = () => {
+  const searchParams = useSearchParams();
+  const username = searchParams.get("username");
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    if (!username) return;
+
+    const fetchReviews = async () => {
+      const q = query(
+        collection(db, "users"),
+        where("username", "==", username)
+      );
+      const querySnapshot = await getDocs(q);
+      const userData = querySnapshot.docs[0]?.data();
+
+      if (userData) {
+        setReviews(userData.reviews || []);
+      }
+    };
+
+    fetchReviews();
+  }, [username]);
+
+  return (
+    <div className="container mx-auto p-4 space-y-8">
+      <h1 className="text-4xl font-bold text-center mb-8">Recent Reviews</h1>
+      {reviews.length > 0 ? (
+        <ul className="space-y-4">
+          {reviews.map((review) => (
+            <li
+              key={review.gameId}
+              className="p-4 border border-gray-300 rounded bg-white shadow-md hover:shadow-lg transition-shadow duration-300"
+            >
+              <Link href={`/games/${review.gameId}`}>
+                <p className="text-xl font-bold hover:underline">
+                  {review.gameName}
+                </p>
+              </Link>
+              <p className="text-yellow-500">
+                {"★".repeat(review.rating)}
+                {"☆".repeat(5 - review.rating)}
+              </p>
+              <p className="mt-2">{review.comment}</p>
+              {review.containsSpoilers && (
+                <p className="text-red-500 mt-1">Contains Spoilers</p>
+              )}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p className="text-lg">You have not written any reviews yet.</p>
+      )}
+    </div>
+  );
+};
+
+export default RecentReviews;
