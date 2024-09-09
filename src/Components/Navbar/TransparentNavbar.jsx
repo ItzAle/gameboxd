345ca37251c443f6a1fd8f3556545be10359981d
ale <alexcs9230@gmail.com>
2024-09-10 00:43:07 +0200

    add api page

--- src/Components/Navbar/TransparentNavbar.jsx
+++ src/Components/Navbar/TransparentNavbar.jsx
@@ -14,6 +14,7 @@ import {
 } from "react-icons/fa";
 import { signOut } from "firebase/auth";
 import { auth } from "../../../lib/firebase";
+import { SiGoogledocs } from "react-icons/si";
 
 export default function TransparentNavbar() {
   const { user } = useAuth(); // Usa useAuth en lugar de useSession
@@ -65,6 +66,11 @@ export default function TransparentNavbar() {
     ...(user
       ? [
           { href: "/profile", icon: <FaUser />, text: "Profile" },
+          {
+            href: "https://gbxd-api.vercel.app/",
+            icon: <SiGoogledocs />,
+            text: "API",
+          },
           {
             href: "#",
             icon: <FaSignOutAlt />,
