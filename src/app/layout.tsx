607426db39b03171591e6213f1dd9279b7fc367e
ale <alexcs9230@gmail.com>
2024-09-09 14:02:22 +0200

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
-import Navbar from "./../Components/Navbar/Navbar";
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
