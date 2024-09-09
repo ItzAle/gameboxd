a0220567e5e6eedf477992d4ae37d25768d73fa2
ale <alexcs9230@gmail.com>
2024-09-09 14:21:42 +0200

    fix

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -5,16 +5,12 @@ import { getFirestore } from "firebase-admin/firestore";
 
 let db;
 
-const privateKey = process.env.FIREBASE_PRIVATE_KEY
-  ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n').replace(/\n/g, '\n')
-  : undefined;
-
 if (!getApps().length) {
   const app = initializeApp({
     credential: cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: privateKey,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY,
     }),
   });
   db = getFirestore(app);
