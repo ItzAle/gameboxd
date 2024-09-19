549fc0fe5c8b8b4713dbdbec08e6c1bf38b57d34
ale <alexcs9230@gmail.com>
2024-09-19 23:23:51 +0200

    add google index

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,12 +7,16 @@ import "react-toastify/dist/ReactToastify.css";
 import { ReviewsProvider } from "../context/ReviewsProvider";
 import { ReactNode } from "react";
 import { AuthProvider } from "../context/AuthContext";
-import Script from 'next/script';
+import Script from "next/script";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
+        <meta
+          name="google-site-verification"
+          content="ObUINvCWZOrJ9bzO0kceilTZ566zmC7B-3WQvTdFW3s"
+        />
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3043119271393042"
