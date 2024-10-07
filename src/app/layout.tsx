a29f77d40f15fa8b074b5ddaca2a57903ff53bcf
ale <alexcs9230@gmail.com>
2024-10-07 12:24:29 +0200

    remove

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,13 +29,6 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <head>
         <meta name="monetag" content="32873ca5de19e872c956483b3b0cc0a6" />
       </head>
-      <Script
-        src="https://alwingulla.com/88/tag.min.js"
-        data-zone="106453"
-        strategy="afterInteractive"
-        async
-        data-cfasync="false"
-      />
       <body className="flex flex-col min-h-screen bg-gradient-to-b from-gray-900 to-black text-white">
         <AuthProvider>
           <SessionProvider>
